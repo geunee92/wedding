82d9a2973d4d57526a18011c5b9a78c70c682b48
Hyungeun Jo <geunee92@ship-da.com>
2025-02-18 13:46:03 +0900

    feat: main video 영상 추가

--- src/components/sections/Video.tsx
+++ src/components/sections/Video.tsx
@@ -5,7 +5,19 @@ import classNames from 'classnames/bind'
 const cx = classNames.bind(styles)
 
 function Video() {
-  return <Section>Video</Section>
+  return (
+    <Section className={cx('container')}>
+      <video
+        // autoPlay={true}
+        muted={true}
+        loop={true}
+        // poster="/assets/poster.jpg"
+        controls={true}
+      >
+        <source src="/assets/medium.mp4" type="video/mp4" />
+      </video>
+    </Section>
+  )
 }
 
 export default Video
