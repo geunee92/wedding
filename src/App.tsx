95eb7f9b836eef409a2e23dfe795e51f1b9cba38
Hyungeun Jo <geunee92@ship-da.com>
2025-03-15 15:44:45 +0900

    refactor: 웨딩 데이터 불러오기 커스텀훅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import classNames from 'classnames/bind'
 
 import styles from './App.module.scss'
-import { useEffect, useState } from 'react'
 
 import Heading from '@components/sections/Heading'
 import Video from '@components/sections/Video'
 import FullScreenMessage from '@shared/FullScreenMessage'
-import { Wedding } from './types/wedding'
 import ImageGallery from './components/sections/ImageGallery'
 import IntroMessage from './components/sections/IntroMessage'
 import Invitation from './components/sections/Invitation'
@@ -15,35 +13,12 @@ import Map from './components/sections/Map'
 import Contact from './components/sections/Contact'
 import Share from './components/sections/Share'
 import AttendCountModal from './components/AttendCountModal.tsx'
+import useWedding from './hooks/useWedding.ts'
 
 const cx = classNames.bind(styles)
 
 function App() {
-  const [wedding, setWedding] = useState<Wedding | null>(null)
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(false)
-
-  useEffect(() => {
-    setLoading(true)
-
-    fetch('http://localhost:8888/wedding')
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error('청첩장 정보를 불러오지 못했습니다.')
-        }
-        return res.json()
-      })
-      .then((data) => {
-        setWedding(data)
-      })
-      .catch((e) => {
-        console.log('에러발생', e)
-        setError(true)
-      })
-      .finally(() => {
-        setLoading(false)
-      })
-  }, [])
+  const { wedding, loading, error } = useWedding()
 
   if (loading) {
     return <FullScreenMessage type={'loading'} />
